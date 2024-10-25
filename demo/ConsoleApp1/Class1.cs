bd824e182ab3fb61978954bcc2f6bd797f881611
bihuwen <b2397699441@163.com>
2024-10-25 13:57:36 +0800

    demo

---
+++ demo/ConsoleApp1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public string GetClassType(Teacher teacher)
+        {
+        return teacher.GetType().Name;
+        }
+        public string GetClassType(Student student)
+        {
+            return student.GetType().Name;
+        }
+        public string GetClassType(Worker worker)
+        {
+            return worker.GetType().Name;
+        }
+        public int GetInt(string str) 
+        {
+            return int.Parse(str);
+        }
+        public float GetFL(string str)
+        {
+            return float.Parse(str);
+        }
+    }
+}
---
+++ demo/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1;
+using ConsoleApp1.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //没有泛型的情况下
+        Class1 noMethod=new Class1();
+        Teacher teacher=new Teacher();
+        string teaTypeName=noMethod.GetClassType(teacher);
+        Console.WriteLine(teaTypeName);
+        Student student = new Student();
+        string stuTypeName = noMethod.GetClassType(student);
+        Console.WriteLine(stuTypeName);
+    }
+}
